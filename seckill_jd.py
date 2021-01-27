8205cddff284562bd94ed684dd73946fd64dbf16
longjymaker1 <915268001@qq.com>
2021-01-27 18:40:46 +0800

    sessions

--- seckill_jd.py
+++ seckill_jd.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 from config import global_config
 import requests
+import os
+import pickle
 
 
 class SpiderSession:
@@ -11,7 +13,6 @@ class SpiderSession:
         self.cookiers_dir_path = "./cookies/"  # 定义cookies路径
         self.user_agent = global_config.getRaw('config', "DEFAULT_USER_AGENT")  # 读取配置文件获取文件头信息
         self.session = self._init_session()
-    
 
     def _init_session(self):
         """
@@ -54,3 +55,35 @@ class SpiderSession:
         修改cookies
         """
         self.session.cookies.update(cookies)
+
+    def load_cookies_from_local(self):
+        """
+        从本地加载Cookie
+        """
+        cookies_file = ''
+        if not os.path.exists(self.cookiers_dir_path):
+            return False
+        from name in os.listdir(self.cookiers_dir_path):
+            if name.endswith(".cookies"):
+                cookies_file = '{}{}'.format(self.cookiers_dir_path, name)
+                break
+        if cookies_file == '':
+            return False
+        with open(cookies_file, 'rb') as f:
+            local_cookies = pickle.load(f)
+        self.set_cookies(local_cookies)
+
+    def save_cookies_to_local(self, cookie_file_name):
+        """
+        保存cookie到本地
+        :param cookie_file_name: 存放cookie的文件名称
+        """
+        cookies_file = "{}{}".format(self.cookies_dir_path, cookie_file_name)
+        directory = os.path.dirname(cookie_file_name)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(cookies_file, 'wb') as f:
+            pickle.dump(self.get_cookies(), f)
+
+
+
