e2e33dc8fec3ceb6a27a14652f7ab8e321244efe
longjymaker1 <915268001@qq.com>
2021-01-30 16:20:20 +0800

    ...

--- seckill_jd.py
+++ seckill_jd.py
@@ -6,13 +6,16 @@ import requests
 import os
 import pickle
 import time
+from lxml import etree
 from logger import logger
+from timer import Timer
 from util import (
     response_status,
     save_image,
     open_image,
     parse_json,
-    SKException
+    SKException,
+    wait_some_time
 )
 
 
@@ -90,8 +93,8 @@ class SpiderSession:
         保存cookie到本地
         :param cookie_file_name: 存放cookie的文件名称
         """
-        cookies_file = "{}{}".format(self.cookies_dir_path, cookie_file_name)
-        directory = os.path.dirname(cookie_file_name)
+        cookies_file = "{}{}".format(self.cookiers_dir_path, cookie_file_name)
+        directory = os.path.dirname(cookies_file)
         if not os.path.exists(directory):
             os.makedirs(directory)
         with open(cookies_file, 'wb') as f:
@@ -256,11 +259,283 @@ class JdSeckill:
         self.seckill_init_info = dict()
         self.seckill_url = dict()
         self.seckill_order_data = dict()
-        self.times = Timer()
+        self.timers = Timer()
+
+        self.session = self.spider_session.get_session()
+        self.user_agent = self.spider_session.get_user_agent()
+        self.nick_name = None
+
+    def login_by_jdcode(self):
+        """扫码登录"""
+        if self.jdlogin.is_login:
+            logger.info('登录成功')
+            return
+
+        self.jdlogin.login_by_jdcode()
+
+        if self.jdlogin.is_login:
+            self.nick_name = self.get_username()  # 获取登录用户名
+            self.spider_session.save_cookies_to_local(self.nick_name)
+        else:
+            raise SKException("二维码登录失败")
+
+    def check_login(func):
+        """
+        用户登录状态装饰器，如果用户没登录，调用扫码登录
+        """
+        def new_func(self, *args, **kwargs):
+            if not self.jdlogin.is_login:
+                logger.info("{0} 需登陆后调用，开始扫码登陆".format(func.__name__))
+                self.login_by_jdcode()
+                # is_effective  # 判断是否有效
+            return func(self, *args, **kwargs)
+        return new_func
+
+    def get_username(self):
+        """获取用户信息"""
+        url = 'https://passport.jd.com/user/petName/getUserInfoForMiniJd.action'
+        payload = {
+            'callback': 'jQuery{}'.format(random.randint(1000000, 9999999)),
+            '_': str(int(time.time() * 1000)),
+        }
+        headers = {
+            'User-Agent': self.user_agent,
+            'Referer': 'https://order.jd.com/center/list.action',
+        }
+
+        resp = self.session.get(url=url, params=payload, headers=headers)
+
+        try_count = 5
+        while not resp.text.startswith("jQuery"):
+            try_count = try_count - 1
+            if try_count > 0:
+                resp = self.session.get(url=url, params=payload, headers=headers)
+            else:
+                break
+            wait_some_time()
+            # jQuery2381773({"imgUrl":"//storage.360buyimg.com/i.imageUpload/xxx.jpg",
+            # "lastLoginTime":"","nickName":"xxx","plusStatus":"0","realName":"xxx","userLevel":x,
+            # "userScoreVO":{"accountScore":xx,"activityScore":xx,"consumptionScore":xxxxx,
+            # "default":false,"financeScore":xxx,"pin":"xxx","riskScore":x,"totalScore":xxxxx}})
+        return parse_json(resp.text).get('nickName')
+
+    def get_sku_title(self):
+        """获取商品名称"""
+        url = 'https://item.jd.com/{}.html'.format(global_config.getRaw('config', 'sku_id'))
+        resp = self.session.get(url).content
+        x_data = etree.HTML(resp)
+        sku_title = x_data.xpath('/html/head/title/text()')
+        return sku_title[0]
+
+    @check_login
+    def reserve(self):
+        """预约"""
+        self._reserve()
+
+    def _reserve(self):
+        """预约"""
+        while True:
+            try:
+                self.make_reserve()
+                break
+            except Exception as e:
+                logger.info('预约发生异常！！！', e)
+            wait_some_time()
+
+    def make_reserve(self):
+        url = 'https://yushou.jd.com/youshouinfo.action?'
+        payload = {
+            'callback': 'fetchJSON',
+            'sku': self.sku_id,
+            '_': str(int(time.time() * 1000)),
+        }
+        headers = {
+            'User-Agent': self.user_agent,
+            'Referer': 'https://item.jd.com/{}.html'.format(self.sku_id),
+        }
+        resp = self.session.get(url=url, params=payload, headers=headers)
+        resp_json = parse_json(resp.text)
+        reserve_url = resp_json.get('url')
+        self.timers.start()
+        while True:
+            try:
+                self.session.get(url='https:' + reserve_url)
+                logger.info('预约成功, 已经获得抢购资格 / 已经成功预约过了,无需重复预约')
+                # success_message = "预约成功，已获得抢购资格 / 您已成功预约过了，无需重复预约"
+                # send_wechat(success_message)  # 微信提醒
+                break
+            except Exception as e:
+                logger.error('预约失败, 正在重试... ...')
+
+    @check_login
+    def seckill(self):
+        """抢购"""
+        self._seckill()
+
+    def _seckill(self):
+        """抢购"""
+        while True:
+            try:
+                self.request_seckill_url()  # 访问商品链接
+                while True:
+                    self.request_seckill_checkout_pate()  # 访问抢购订单结算页面
+                    self.submit_seckill_order()  # 提交抢购（秒杀）订单
+            except Exception as e:
+                logger.info('抢购发生异常, 稍后继续执行', e)
+            wait_some_time()
+
+    def get_seckill_url(self):
+        """
+        获取商品的抢购链接
+        点击"抢购"按钮后，会有两次302跳转，最后到达订单结算页面
+        这里返回第一次跳转后的页面url，作为商品的抢购链接
+        :return: 商品的抢购链接
+        """
+        url = 'https://itemko.jd.com/itemShowBtn'
+        payload = {
+            'callback': 'jQuery{}'.format(random.randint(1000000, 9999999)),
+            'skuId': self.sku_id,
+            'from': 'pc',
+            '_': str(int(time.time() * 1000)),
+        }
+        headers = {
+            'User-Agent': self.user_agent,
+            'Host': 'itemko.jd.com',
+            'Referer': 'https://item.jd.com/{}.html'.format(self.sku_id),
+        }
+        while True:
+            resp = self.session.get(url=url, headers=headers, params=payload)
+            resp_json = parse_json(resp.text)
+            if resp_json.get('url'):
+                router_url = 'https:' + resp_json.get('url')
+                seckill_url = router_url.replace(
+                    'divide', 'marathon'
+                ).replace(
+                    'user_routing', 'captcha.html'
+                )
+                logger.info('抢购链接获取成功: %s', seckill_url)
+                return seckill_url
+            else:
+                logger.info('抢购链接获取失败, 稍后重试')
+                wait_some_time()
+
+    def request_seckill_url(self):
+        """访问商品的抢购链接, 用于设置cookie等"""
+        logger.info('用户:{}'.format(self.get_username()))
+        logger.info('商品名称:{}'.format(self.get_sku_title()))
+        self.timers.start()
+        self.seckill_url[self.sku_id] = self.get_seckill_url()
+        logger.info('访问商品抢购链接...')
+        headers = {
+            'User-Agent': self.user_agent,
+            'Host': 'marathon.jd.com',
+            'Referer': 'https://item.jd.com/{}.html'.format(self.sku_id),
+        }
+        self.session.get(
+            url=self.seckill_url.get(self.sku_id),
+            headers=headers,
+            allow_redirects=False
+        )
+
+    def request_seckill_checkout_pate(self):
+        """访问抢购订单结算页面"""
+        logger.info('访问抢购订单结算页面...')
+        url = 'https://marathon.jd.com/seckill/seckill.action'
+        payload = {
+            'skuId': self.sku_id,
+            'num': self.seckill_num,
+            'rid': int(time.time())
+        }
+        headers = {
+            'User-Agent': self.user_agent,
+            'Host': 'marathon.jd.com',
+            'Referer': 'https://item.jd.com/{}.html'.format(self.sku_id),
+        }
+        self.session.get(url=url, params=payload, headers=headers, allow_redirects=False)
+
+    def _get_seckill_init_info(self):
+        """
+        获取秒杀初始化信息, 地址, 发票, token
+        :return: 初始化信息组成的dict
+        """
+        logger.info('获取藐视啊初始化信息...')
+        url = 'https://marathon.jd.com/seckillnew/orderService/pc/init.action'
+        data = {
+            'sku': self.sku_id,
+            'num': self.seckill_num,
+            'isModifyAddress': 'false',
+        }
+        headers = {
+            'User-Agent': self.user_agent,
+            'Host': 'marathon.jd.com',
+        }
+        resp = self.session.post(url=url, data=data, headers=headers)
+
+        resp_json = None
+        try:
+            resp_json = parse_json(resp.text)
+        except Exception:
+            raise SKException('抢购失败，返回信息:{}'.format(resp.text[0: 128]))
+        return resp_json
+
+    def _get_seckill_order_data(self):
+        """
+        生成提交抢购订单锁余姚的请求参数
+        :return: 请求体参数组成的dict
+        """
+        logger.info('生成提交抢购订单所需参数...')
+        # 获取用户秒杀初始化信息
+        self.seckill_init_info[self.sku_id] = self._get_seckill_init_info()
+        init_info = self.seckill_init_info.get(self.sku_id)
+        default_address = init_info['addressList'][0]  # 默认地址dict
+        invoice_info = init_info.get('invoiceInfo', {})  # 默认发票信息dict, 有可能不返回
+        token = init_info['token']
+        data = {
+            'skuId': self.sku_id,
+            'num': self.seckill_num,
+            'addressId': default_address['id'],
+            'yuShou': 'true',
+            'isModifyAddress': 'false',
+            'name': default_address['name'],
+            'provinceId': default_address['provinceId'],
+            'cityId': default_address['cityId'],
+            'countyId': default_address['countyId'],
+            'townId': default_address['townId'],
+            'addressDetail': default_address['addressDetail'],
+            'mobile': default_address['mobile'],
+            'mobileKey': default_address['mobileKey'],
+            'email': default_address.get('email', ''),
+            'postCode': '',
+            'invoiceTitle': invoice_info.get('invoiceTitle', -1),
+            'invoiceCompanyName': '',
+            'invoiceContent': invoice_info.get('invoiceContentType', 1),
+            'invoiceTaxpayerNO': '',
+            'invoiceEmail': '',
+            'invoicePhone': invoice_info.get('invoicePhone', ''),
+            'invoicePhoneKey': invoice_info.get('invoicePhoneKey', ''),
+            'invoice': 'true' if invoice_info else 'false',
+            'password': global_config.get('account', 'payment_pwd'),
+            'codTimeType': 3,
+            'paymentType': 4,
+            'areaCode': '',
+            'overseas': 0,
+            'phone': '',
+            'eid': global_config.getRaw('config', 'eid'),
+            'fp': global_config.getRaw('config', 'fp'),
+            'token': token,
+            'pru': ''
+        }
+        return data
+
+    def submit_seckill_order(self):
+        """
+        提交抢购（秒杀）订单
+        :return: 抢购结果 True/False
+        """
+        pass
 
 
 if __name__ == "__main__":
-    spids = SpiderSession()
-    sc = JdLogin(spids)
-    print(sc.login_by_jdcode())
     # unick = jd915268rii
+    jds = JdSeckill()
+    jds.reserve()
--- timer.py
+++ timer.py
@@ -8,6 +8,52 @@ from logger import logger
 from config import global_config
 
 
-class Time:
+class Timer:
     def __init__(self, sleep_interval=0.5):
         buy_time_everyday = global_config.getRaw('config', 'buy_time')
+        localtime = time.localtime(time.time())
+        self.buy_time = datetime.strptime(
+            localtime.tm_year.__str__() + '-' + localtime.tm_mon.__str__() + '-' + localtime.tm_mday.__str__()
+            + ' ' + buy_time_everyday, "%Y-%m-%d %H:%M:%S.%f"
+        )
+        # 购买时间转化为毫秒
+        self.buy_time_ms = int(time.mktime(self.buy_time.timetuple()) * 1000.0 + self.buy_time.microsecond / 1000)
+        self.sleep_interval = sleep_interval
+
+        self.diff_time = self.local_jd_dime_diff()
+
+    @staticmethod
+    def jd_time():
+        """
+        从京东服务器获取时间毫秒
+        """
+        url = 'https://a.jd.com//ajax/queryServerData.html'
+        ret = requests.get(url).text
+        js = json.loads(ret)
+        return int(js["serverTime"])
+
+    @staticmethod
+    def local_time():
+        """获取本地时间"""
+        return int(round(time.time() * 1000))
+
+    def local_jd_dime_diff(self):
+        """计算本地时间与京东服务器时间差"""
+        return self.local_time() - self.jd_time()
+
+    def start(self):
+        """根据本地时间与京东服务时间等待运行"""
+        logger.info("正在等待到达设定时间: {}, 检测本地时间与京东服务器时间误差【{}】毫秒".format(self.buy_time, self.diff_time))
+        while True:
+            if self.local_time() - self.diff_time >= self.buy_time_ms:
+                logger.info("时间到达, 执行开始... ...")
+                break
+            else:
+                time.sleep(self.sleep_interval)
+
+
+if __name__ == '__main__':
+    t0 = Timer()
+    print(t0.jd_time())
+    print(t0.local_time())
+    print(t0.local_jd_dime_diff())
