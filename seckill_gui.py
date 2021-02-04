168eb34fedc76a06ebb3eecd6c1770ea92a68eb7
longjymaker1 <915268001@qq.com>
2021-02-04 18:07:27 +0800

    gui

--- seckill_gui.py
+++ seckill_gui.py
@@ -1,4 +1,19 @@
 # -*- encoding: utf-8 -*-
-import tkinter
+from tkinter import *
 
+root = Tk()
 
+li = ['C', 'python', 'php', 'html', 'SQL', 'java']
+movie = ['CSS', 'jQuery', 'Bootstrap']
+
+listb = Listbox(root)  # 创建两个列表组件
+listb2 = Listbox(root)
+for item in li:  # 第一个小部件插入数据
+    listb.insert(0, item)
+
+for item in movie:  # 第二个小部件插入数据
+    listb2.insert(0, item)
+
+listb.pack()  # 将小部件放置到主窗口中
+listb2.pack()
+root.mainloop()  # 进入消息循环
